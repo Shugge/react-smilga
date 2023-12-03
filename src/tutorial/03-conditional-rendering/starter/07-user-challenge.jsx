11b46d31416112c34c7c357d39355f9a22ee90c3
Shugge <deandersson22@gmail.com>
2023-12-03 22:01:01 +0100

    senaste

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({name: 'John Doe'});
+  }
+
+  const logout = () => {
+    setUser(null);
+  }
+
+  return (
+    <section>
+      {user ? 
+        <div>
+          <h4>Hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>Logout</button>
+        </div>
+      :
+        <div>
+          <h4>Please login</h4>
+          <button className="btn" onClick={login}>Login</button>
+        </div>
+      }
+    </section>
+  );
 };
 
 export default UserChallenge;
